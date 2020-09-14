f78442cfc2b515efa049f0ec8db63ad222018a3a
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-14 23:24:46 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,7 +18,8 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 
+    s +
+    return rad
 
 
 def to_degrees(angle_rads: float) -> tuple:
