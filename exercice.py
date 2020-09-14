0a79efcf770324ad46559d8748f9170d45e18e57
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-14 23:25:11 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,7 +18,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    angle = ang
+    angle = angle_degs + angle_mins + angle_secs
         return rad
 
 
